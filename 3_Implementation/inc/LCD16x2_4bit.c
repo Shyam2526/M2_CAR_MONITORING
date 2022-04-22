b785296983633ea55f9ca182e71d2b4da2175c93
shyamkumar <shyamakumar4516@gmail.com>
2022-04-22 18:53:23 +0530

    files updated

--- 3_Implementation/inc/LCD16x2_4bit.c
+++ 3_Implementation/inc/LCD16x2_4bit.c
@@ -1,23 +1,17 @@
-/*
- * LCD16x2_4bit.c
- *
- * http://www.electronicwings.com
- */ 
-
 #include "LCD16x2_4bit.h"
 
 
 void LCD_Command( unsigned char cmnd )
 {
-	LCD_Port = (LCD_Port & 0x0F) | (cmnd & 0xF0); /* sending upper nibble */
-	LCD_Port &= ~ (1<<RS);				/* RS=0, command reg. */
-	LCD_Port |= (1<<EN);				/* Enable pulse */
+	LCD_Port = (LCD_Port & 0x0F) | (cmnd & 0xF0);
+	LCD_Port &= ~ (1<<RS);
+	LCD_Port |= (1<<EN);
 	_delay_us(1);
 	LCD_Port &= ~ (1<<EN);
 
 	_delay_us(200);
 
-	LCD_Port = (LCD_Port & 0x0F) | (cmnd << 4);  /* sending lower nibble */
+	LCD_Port = (LCD_Port & 0x0F) | (cmnd << 4);
 	LCD_Port |= (1<<EN);
 	_delay_us(1);
 	LCD_Port &= ~ (1<<EN);
@@ -27,58 +21,58 @@ void LCD_Command( unsigned char cmnd )
 
 void LCD_Char( unsigned char data )
 {
-	LCD_Port = (LCD_Port & 0x0F) | (data & 0xF0); /* sending upper nibble */
-	LCD_Port |= (1<<RS);				/* RS=1, data reg. */
+	LCD_Port = (LCD_Port & 0x0F) | (data & 0xF0);
+	LCD_Port |= (1<<RS);
 	LCD_Port|= (1<<EN);
 	_delay_us(1);
 	LCD_Port &= ~ (1<<EN);
 
 	_delay_us(200);
 
-	LCD_Port = (LCD_Port & 0x0F) | (data << 4); /* sending lower nibble */
+	LCD_Port = (LCD_Port & 0x0F) | (data << 4);
 	LCD_Port |= (1<<EN);
 	_delay_us(1);
 	LCD_Port &= ~ (1<<EN);
 	_delay_ms(2);
 }
 
-void LCD_Init (void)					/* LCD Initialize function */
+void LCD_Init (void)
 {
-	LCD_Dir = 0xFF;						/* Make LCD command port direction as o/p */
-	_delay_ms(20);						/* LCD Power ON delay always >15ms */
-	
+	LCD_Dir = 0xFF;
+	_delay_ms(20);
+
 	LCD_Command(0x33);
-	LCD_Command(0x32);		    		/* send for 4 bit initialization of LCD  */
-	LCD_Command(0x28);              	/* Use 2 line and initialize 5*7 matrix in (4-bit mode)*/
-	LCD_Command(0x0c);              	/* Display on cursor off*/
-	LCD_Command(0x06);              	/* Increment cursor (shift cursor to right)*/
-	LCD_Command(0x01);              	/* Clear display screen*/
+	LCD_Command(0x32);
+	LCD_Command(0x28);
+	LCD_Command(0x0c);
+	LCD_Command(0x06);
+	LCD_Command(0x01);
 	_delay_ms(2);
-	LCD_Command (0x80);					/* Cursor 1st row 0th position */
+	LCD_Command (0x80);
 }
 
 
-void LCD_String (char *str)				/* Send string to LCD function */
+void LCD_String (char *str)
 {
 	int i;
-	for(i=0;str[i]!=0;i++)				/* Send each char of string till the NULL */
+	for(i=0;str[i]!=0;i++)
 	{
 		LCD_Char (str[i]);
 	}
 }
 
-void LCD_String_xy (char row, char pos, char *str)	/* Send string to LCD with xy position */
+void LCD_String_xy (char row, char pos, char *str)
 {
 	if (row == 0 && pos<16)
-	LCD_Command((pos & 0x0F)|0x80);		/* Command of first row and required position<16 */
+	LCD_Command((pos & 0x0F)|0x80);
 	else if (row == 1 && pos<16)
-	LCD_Command((pos & 0x0F)|0xC0);		/* Command of first row and required position<16 */
-	LCD_String(str);					/* Call LCD string function */
+	LCD_Command((pos & 0x0F)|0xC0);
+	LCD_String(str);
 }
 
 void LCD_Clear()
 {
-	LCD_Command (0x01);					/* Clear display */
+	LCD_Command (0x01);
 	_delay_ms(2);
-	LCD_Command (0x80);					/* Cursor 1st row 0th position */
+	LCD_Command (0x80);
 }
--- 3_Implementation/inc/LCD16x2_4bit.h
+++ 3_Implementation/inc/LCD16x2_4bit.h
@@ -1,21 +1,15 @@
-/*
- * LCD16x2_4bit.h
- *
- * http://www.electronicwings.com
- */ 
-
 
 #ifndef LCD16X2_4BIT_H_
 #define LCD16X2_4BIT_H_
 
-#define F_CPU 8000000UL					/* Define CPU Frequency e.g. here its 8MHz */
-#include <avr/io.h>						/* Include AVR std. library file */
-#include <util/delay.h>					/* Include inbuilt defined Delay header file */
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
 
-#define LCD_Dir DDRB					/* Define LCD data port direction */
-#define LCD_Port PORTB					/* Define LCD data port */
-#define RS PB0							/* Define Register Select (data reg./command reg.) signal pin */
-#define EN PB1 							/* Define Enable signal pin */
+#define LCD_Dir DDRB
+#define LCD_Port PORTB
+#define RS PB0
+#define EN PB1
 
 void LCD_Init ();
 void LCD_Clear();
@@ -24,4 +18,4 @@ void LCD_Char( unsigned char data );
 void LCD_Command( unsigned char cmnd );
 void LCD_String_xy (char row, char pos, char *str);
 
-#endif /* LCD16X2_4BIT_H_ */
+#endif
